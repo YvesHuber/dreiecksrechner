7f872c79663cee0202d02b722fe505cbb7e42f4f
YvesHuber <cpt.rino@gmail.com>
2021-09-24 14:28:59 +0200

    added first function

--- src/dreieck.js
+++ src/dreieck.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function Dreieck() {
 
 
-    
+
+    const [alpha, setalpha] = useState(0);
+    const [beta, setbeta] = useState(0);
+    const [gamma, setgamma] = useState(0);
+
+    function calculategamma(){
+        setgamma(180-alpha-beta);
+    }
+
+
   return (
     <div className="App">
       <header className="App-header">
       
+        <lable>Alpha</lable>
+      <input type="text" value={alpha} onChange={ (e) => {if(e.target.value <= 178){
+          setalpha(e.target.value)
+      }}}/>
+      <lable>Beta</lable>
+      <input type="text" value={beta} onChange={ (e) => {if(e.target.value <= 178){
+          setbeta(e.target.value)
+      }}}/>
+      <button type="button" value="calculate" onClick={calculategamma}>calculate </button>
 
-      <input type="text"/>
-      <input type="text"/>
-
-      <p></p>
-      <p></p>
-      <p></p>
+      <p>Alpha = {alpha}</p>
+      <p>Beta = {beta}</p>
+      <p>Gamma = {gamma}</p>
 
         
       </header>
