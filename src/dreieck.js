f9b280c7f1a5e757c04a3bf4d46f282126ed4171
Yves Huber <yves.huber@lernende.bbw.ch>
2021-11-05 13:49:16 +0100

    calculation is now possible

--- src/dreieck.js
+++ src/dreieck.js
@@ -7,56 +7,44 @@ function Dreieck() {
   const scene = new THREE.Scene();
 
   const formuladata =[
-    {Value: "c", Formula: "√ A^2 + B^2", id: 0, inputname1: "Kathete", inputname2: "Kathete", outputname: "Hypothenuse", input1: 0, input2: 0, result: 0},
-    {Value: "a", Formula: "√ C^2 - B^2", id: 1},
-    {Value: "alpha", Formula: "180 - gamma - beta", id: 2},
-    {Value: "gamma", Formula: "180- alpha - beta", id: 3},
-    {Value: "height", Formula: "2 * a / b", id: 4},
+    {Value: "c", Formula: "√ A^2 + B^2", id: 1},
+    {Value: "a", Formula: "√ C^2 - B^2", id: 2},
+    {Value: "alpha", Formula: "180 - gamma - beta", id: 3},
+    {Value: "gamma", Formula: "180 - alpha - beta", id: 4},
+    {Value: "height", Formula: "2 * a / b", id: 3},
     ];
-    const [selectFormulas, setselectFormulas] = useState(formuladata);
-    const [selectedFormula, setselectedFormula] = useState({Value: "", Formula: ""});
+    const [Formulas, setFormulas] = useState(formuladata);
+    const [formulaid, setformulaid] = useState(0);
 
-    const [alphaangle, setalphaangle] = useState(0);
-    const [betaangle, setbetaangle] = useState(0);
-    const [gammaangle, setgammaangle] = useState(0);
+    const [input1, setinput1] = useState(0);
+    const [input2, setinput2] = useState(0);
+    const [result, setresult] = useState(0);
 
-    const [alength, setalength] = useState(0);
-    const [blength, setblength] = useState(0);
-    const [clength, setclength] = useState(0);
 
-    function calculate(){
-      switch(selectedFormula.Value){
-        case 0:
-          //calculate the hypotenuse
-          selectedFormula.result = 180 - selectedFormula.input1 - selectedFormula.input2;
-          return;
-        case 1:
-          //calculate the kathete
-          return;
-        case 2:
-          //calculate alphaangle
-          return; 
-        case 3:
-          //calculate gammaangle
-          return; 
-        case 4:
-          // calculate the height
-          return;
+
+
+    function calculate(){      
+      if (formulaid == 1){
+        //calculate hypothenuse
+        setresult(Math.sqrt( Math.pow(input1, 2) + Math.pow(input2,2)));
+        console.log(result + " result");
       }
-      calculateangle();
-      calculatelength();
-      
-    }
+      else if (formulaid == 2){
+        //calculate the kathete
+        setresult(Math.sqrt( Math.pow(input1, 2) - Math.pow(input2,2)));
+        console.log(result + " result");
+      }      
+      else if (formulaid == 3 || formulaid == 4){
+        //calculate angle
+        setresult(180 - input1 - input2);
+        console.log(result + " result");
+      }      
+      else if (formulaid == 5){
+        //calculate hypothenuse
+        setresult( 2 * input1 / input2);
+      }
 
-    function calculateangle(){
-      let result;
-      result = 180-alphaangle-betaangle;
-        setgammaangle(result);
-    }
-    function calculatelength(){
-      let result;
-      result = Math.sqrt(Math.pow(alength,2) + Math.pow(blength,2));
-      setclength(Math.round(result));
+
     }
     function draw(){
       const renderer = new THREE.WebGLRenderer();
@@ -93,41 +81,16 @@ function Dreieck() {
   return (
     <div className="App">
       <header className="App-header">
-      <select>
-      <option>What do you want to calculate</option>
-          { selectFormulas.map ( (object, i) => (<option key={i} name="formula" placeholder={object.Value} value={object.Value} onChange={ (e) => setselectedFormula(object)}/>))}
-      <h4>Current formula = {selectedFormula.Formula}</h4>
+        <h1>What do you want to Calculate?</h1>
+      <select onChange={ (e) => setformulaid(e.target.value)}>
+          { 
+  Formulas.map ( (object, i) => (<option key={i} name="formula" value={object.id} >{object.Value}</option>))}
       </select>
-      
-      <lable>Alpha Winkel</lable>
-      <input type="number" value={alphaangle} onChange={ (e) => {if(e.target.value <= 178){
-          setalphaangle(e.target.value)
-      }}}/>
-      <lable>Beta Winkel</lable>
-      <input type="number" value={betaangle} onChange={ (e) => {if(e.target.value <= 178){
-          setbetaangle(e.target.value)
-      }}}/>
-      <lable>A länge </lable>
-      <input type="number" value={alength} onChange={ (e) => {
-          setalength(e.target.value)
-      }}/>
-      <lable>B länge </lable>
-      <input type="number" value={blength} onChange={ (e) => {
-          setblength(e.target.value)
-      }}/>
+
+      <input onChange={ (e) => setinput1(e.target.value)}/>
+      <input onChange={ (e) => setinput2(e.target.value)}/>
       <button type="button" value="calculate" onClick={calculate}>calculate </button>
-
-      <p>Alpha° = {alphaangle}</p>
-      <p>Beta° = {betaangle}</p>
-      <p>Gamma° = {gammaangle}</p>
-
-    
-
-
-      
-
-
-        
+      <p>{result}</p>
       </header>
     </div>
   );
